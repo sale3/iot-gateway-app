7050a14ddfdbc3c2c6f86ce25fa432c66083ea12
Stefan Kapetanovic <stekap000@gmail.com>
2024-04-10 11:27:02 +0200

    Base for adding more test cases for stats service

--- tests/test_stats_service.py
+++ tests/test_stats_service.py
@@ -1,6 +1,5 @@
 import unittest
 import pytest
-import time
 from src.stats_service import Stats, OverallStats
 
 
@@ -35,56 +34,60 @@ class TestStatsService(object):
     ])
     def test_stats_update_data_wrong(self, bytes, forwarded, requests):
         stats = Stats()
-        self.TC.assertRaises(Exception, stats.update_data, bytes, forwarded, requests)
+        self.TC.assertRaises(TypeError, stats.update_data, bytes, forwarded, requests)
 
-    @pytest.mark.parametrize('temp_stats_arr,load_stats_arr,fuel_stats_arr,time_format', [
-        ([1, 1, 1], [2, 2, 2], [3, 3, 3], "%d.%m.%Y %f %a %qq %g %l %H:%M:%S")
+    @pytest.mark.parametrize('temp_stats_arr,load_stats_arr,fuel_stats_arr', [
+        ([1, [], 1], [2, 2, 2], [3, 3, 3]),
+        ([1, 1, 1], [2, 2, 2], [3, 'asd', 3]),
+        ([1, 1, 1], [2, '2', 2], [3, 3, 3])
     ])
-    def test_overall_stats_combine_stats_wrong(self,
-                                               temp_stats_arr,
-                                               load_stats_arr,
-                                               fuel_stats_arr,
-                                               time_format):
-        """
-        self.tempDataBytes = temp_stats.dataBytes
-        self.tempDataBytesForwarded = temp_stats.dataBytesForwarded
-        self.tempDataRequests = temp_stats.dataRequests
-        self.loadDataBytes = load_stats.dataBytes
-        self.loadDataBytesForwarded = load_stats.dataBytesForwarded
-        self.loadDataRequests = load_stats.dataRequests
-        self.fuelDataBytes = fuel_stats.dataBytes
-        self.fuelDataBytesForwarded = fuel_stats.dataBytesForwarded
-        self.fuelDataRequests = fuel_stats.dataRequests
-
-        self.endTime = time.strftime(self.time_pattern, time.localtime())
-        payload = {"startTime": self.startTime,
-                   "endTime": self.endTime,
-                   "tempDataBytes": self.tempDataBytes,
-                   "tempDataBytesForwarded": self.tempDataBytesForwarded,
-                   "tempDataRequests": self.tempDataRequests,
-                   "loadDataBytes": self.loadDataBytes,
-                   "loadDataBytesForwarded": self.loadDataBytesForwarded,
-                   "loadDataRequests": self.loadDataRequests,
-                   "fuelDataBytes": self.fuelDataBytes,
-                   "fuelDataBytesForwarded": self.fuelDataBytesForwarded,
-                   "fuelDataRequests": self.fuelDataRequests}
-        return payload
-        """
+    def test_overall_stats_combine_stats_wrong_stats_input(self,
+                                                           temp_stats_arr,
+                                                           load_stats_arr,
+                                                           fuel_stats_arr):
         temp_stats = Stats()
-        temp_stats.update_data(temp_stats_arr[0], temp_stats_arr[1], temp_stats_arr[2])
+        temp_stats.dataBytes = temp_stats_arr[0]
+        temp_stats.dataRequests = temp_stats_arr[1]
+        temp_stats.dataBytesForwarded = temp_stats_arr[2]
+
         load_stats = Stats()
-        load_stats.update_data(load_stats_arr[0], load_stats_arr[1], load_stats_arr[2])
+        load_stats.dataBytes = load_stats_arr[0]
+        load_stats.dataRequests = load_stats_arr[1]
+        load_stats.dataBytesForwarded = load_stats_arr[2]
+
         fuel_stats = Stats()
-        fuel_stats.update_data(fuel_stats_arr[0], fuel_stats_arr[1], fuel_stats_arr[2])
+        fuel_stats.dataBytes = fuel_stats_arr[0]
+        fuel_stats.dataRequests = fuel_stats_arr[1]
+        fuel_stats.dataBytesForwarded = fuel_stats_arr[2]
+
+        time_format = "dd.MM.yyyy HH:mm:ss"
 
         overall_stats = OverallStats(time_format)
-        resp = overall_stats.combine_stats(temp_stats, load_stats, fuel_stats)
+        self.TC.assertRaises(TypeError,
+                             overall_stats.combine_stats,
+                             temp_stats,
+                             load_stats,
+                             fuel_stats)
 
-        if resp["endTime"] == time_format:
-            self.TC.fail("Time format string not supported")
-        else:
-            self.TC.assertRaises(ValueError, overall_stats.combine_stats, temp_stats, load_stats, fuel_stats)
+    @pytest.mark.parametrize('time_format', [
+        "%d.%m.%Y %f %a %qq %g %l %H:%M:%S",
+        "asdffb -. asdf"
+    ])
+    def test_overall_stats_wrong_time_format(self,
+                                             time_format):
+        dummy_stats = Stats()
+        dummy_stats.update_data(0, 0, 0)
 
-        f = open("ffff.txt", "w")
-        f.write(time.strftime("", time.localtime()))
-        f.close()
+        has_error = False
+        try:
+            overall_stats = OverallStats(time_format)
+            if overall_stats.startTime == time_format:
+                has_error = True
+            overall_stats.combine_stats(dummy_stats,
+                                        dummy_stats,
+                                        dummy_stats)
+        except ValueError:
+            has_error = True
+
+        if not has_error:
+            self.TC.fail("Invalid time format not caught.")
