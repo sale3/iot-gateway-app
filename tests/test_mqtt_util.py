cfc488763a0818522befbc2e5a0aec589820049a
Stefan Kapetanovic <stekap000@gmail.com>
2024-04-11 14:29:12 +0200

    Additional tests for mqtt_util.py

--- tests/test_mqtt_util.py
+++ tests/test_mqtt_util.py
@@ -4,7 +4,10 @@ import logging
 import json
 import os
 
-from src.mqtt_util import MQTTConf, gcb_init_client
+from src.mqtt_util import MQTTConf, GcbService, \
+    gcb_publisher_on_connect, \
+    gcb_connect, gcb_init_client, gcb_init_publisher, gcb_init_subscriber, \
+    errorLogger, infoLogger
 from src.config_util import Config, CONF_PATH, \
     GATEWAY_CLOUD_BROKER, ADDRESS, PORT, USERNAME, PASSWORD
 
@@ -53,10 +56,76 @@ class TestMqttUtil(object):
         else:
             self.TC.assertIsNone(ret)
 
-    @pytest.mark.parametrize('client_id,username,password', [
-        ("client_id", "username", "password"),
+    @pytest.mark.parametrize('rc', [
+        0, 1, 2, -1, 'a', []
     ])
-    def test_gcb_init_client_correct(self, client_id, username, password):
-        ret = gcb_init_client(client_id, username, password)
-        self.TC.assertIsNotNone(ret)
+    def test_gcb_publisher_on_connect(self, rc):
+        if rc == 0:
+            with self.TC.assertLogs(infoLogger) as info_logger:
+                gcb_publisher_on_connect(0, 0, 0, rc, 0)
+                self.TC.assertEqual(info_logger.output,
+                                    ["INFO:customInfoLogger:Successfully established connection with MQTT broker!"])
+        else:
+            with self.TC.assertLogs(errorLogger) as error_logger:
+                gcb_publisher_on_connect(0, 0, 0, rc, 0)
+                self.TC.assertEqual(error_logger.output,
+                                    ["ERROR:customErrorLogger:Failed to establish connection with MQTT broker!"])
 
+    def test_gcb_subscriber_on_connect(self, rc):
+        if rc == 0:
+            with self.TC.assertLogs(infoLogger) as info_logger:
+                gcb_publisher_on_connect(0, 0, 0, rc, 0)
+                self.TC.assertEqual(info_logger.output,
+                                    ["INFO:customInfoLogger:Successfully established connection with MQTT broker!"])
+        else:
+            with self.TC.assertLogs(errorLogger) as error_logger:
+                gcb_publisher_on_connect(0, 0, 0, rc, 0)
+                self.TC.assertEqual(error_logger.output,
+                                    ["ERROR:customErrorLogger:Failed to establish connection with MQTT broker!"])
+
+    @pytest.mark.parametrize('client_id,username,password', [
+        # From paho mqtt source code, there is no restriction on password type, but there
+        # is on username type which must be string (since encode is called on it).
+        ("client_id", "username", "password"),
+        (1234, "username", "password"),
+        ("client_id", "username", 123124),
+        ("client_id", "username", [])
+    ])
+    def test_gcb_init_functions_correct(self, client_id, username, password):
+        self.TC.assertIsNotNone(gcb_init_client(client_id, username, password))
+        self.TC.assertIsNotNone(gcb_init_publisher(client_id, username, password))
+        self.TC.assertIsNotNone(gcb_init_subscriber(client_id, username, password))
+
+    @pytest.mark.parametrize('client_id,username,password', [
+        ("client_id", 123, "password"),
+        ("client_id", [], "password")
+    ])
+    def test_gcb_init_functions_wrong(self, client_id, username, password):
+        self.TC.assertRaises(AttributeError, gcb_init_client, client_id, username, password)
+        self.TC.assertRaises(AttributeError, gcb_init_publisher, client_id, username, password)
+        self.TC.assertRaises(AttributeError, gcb_init_subscriber, client_id, username, password)
+
+    @pytest.mark.parametrize('topic,data', [
+        ("topic", "data"),
+        (123, "data"),
+        ("topic", 123)
+    ])
+    def test_gcb_service_push_message_correct(self, topic, data):
+        mqtt_conf = MQTTConf("localhost", 1884, "username", "password")
+        gcb_service = GcbService("username", "client_id", mqtt_conf)
+        GcbService.push_message(gcb_service.queue, topic, data)
+        self.TC.assertEqual(gcb_service.queue.qsize(), 1)
+        self.TC.assertEqual({"topic": topic, "data": data}, gcb_service.queue.get())
+
+    def test_gcb_service_publishing_procedure_stopping(self):
+        mqtt_conf = MQTTConf("localhost", 1884, "username", "password")
+        gcb_service = GcbService("username", "client_id", mqtt_conf)
+        gcb_service.stop_flag.set()
+        gcb_service.__publishing_procedure__()
+        self.TC.assertTrue(not gcb_service.stop_flag.is_set())
+
+    def test_gcb_service_stop(self):
+        mqtt_conf = MQTTConf("localhost", 1884, "username", "password")
+        gcb_service = GcbService("username", "client_id", mqtt_conf)
+        gcb_service.stop()
+        self.TC.assertTrue(gcb_service.stop_flag.is_set())
