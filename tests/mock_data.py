72a7a1cf5854511901a7736ed2999e24792cfd8e
Stefan Kapetanovic <stekap000@gmail.com>
2024-04-15 13:25:14 +0200

    mock module cleanup

--- tests/mock_data.py
+++ tests/mock_data.py
@@ -1,9 +1,3 @@
 def create_mock(**kwargs):
     return type("MockObject", (), kwargs)()
 
-if __name__ == "__main__":
-    m = create_mock(a=123,b="asdf")
-    print(m.a)
-    print(m.b)
-    print(bytes(m.a, 'utf-8').decode('utf-8'))
-    print(bytes(m.b, 'utf-8').decode('utf-8'))
