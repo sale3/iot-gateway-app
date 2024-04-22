a80ddbfde33255694db44981fe6c92f2347edc3e
Stefan Kapetanovic <stekap000@gmail.com>
2024-04-22 15:21:54 +0200

    example tests excluded

--- tests/test_data_service.py
+++ tests/test_data_service.py
@@ -11,26 +11,6 @@ from src.data_service import parse_incoming_data, \
 class TestDataService(object):
     TC = unittest.TestCase()
 
-    def test_parse_incoming_data_correct(self):
-        val, typ = parse_incoming_data("nesto shemso=3 indeks_dva bla bla bla halid=invalid", "datatype")
-        assert (val == 3.0)
-        assert (typ == "invalid")
-
-    # TODO: ne raditi ovako nego sa @pytest.mark.parametrize i/ili @fixture
-    def test_parse_incoming_data_wrong(self):
-        # TODO: ovaj zakomentarisani, ne javlja gresku, odnosno ako nesto nije broj dobijemo 0?!?!
-        # TODO: poruke su iste u oba slucaja, treba ih razlikovati
-        # with self.assertLogs('customErrorLogger', level='ERROR') as cm:
-        #     _ = parse_incoming_data("nesto shemso=nije_broj indeks_dva bla bla bla halid=invalid", "datatype")
-        #     self.assertEqual(cm.output, ['ERROR:customErrorLogger:Invalid datatype data format! - nesto shemso indeks_dva bla bla bla halid=invalid'])
-        with self.TC.assertLogs('customErrorLogger', level='ERROR') as cm:
-            _ = parse_incoming_data("nesto shemso=3 indeks_dva bla bla bla halit-ddd", "datatype")
-            self.TC.assertEqual(cm.output, [
-                'ERROR:customErrorLogger:Invalid datatype data format! - nesto shemso=3 indeks_dva bla bla bla halit-ddd'])
-        with self.TC.assertLogs('customErrorLogger', level='ERROR') as cm:
-            _ = parse_incoming_data("nesto shemso=3", "datatype")
-            self.TC.assertEqual(cm.output, ['ERROR:customErrorLogger:Invalid datatype data format! - nesto shemso=3'])
-
     @pytest.mark.parametrize('data', [
         [
             '[ value=-2.0 , time=15.04.2024 14:01:06 , unit=C ]',
