612832303c9dc1f25240cd8cb5888ea05b546c68
Novica Tepic <acivon@outlook.com>
2024-09-04 12:14:01 +0200

    Corrected import

--- src/mqtt_util.py
+++ src/mqtt_util.py
@@ -66,7 +66,7 @@ import logging.config
 import paho.mqtt.client as mqtt
 from threading import Thread, Event
 from queue import Queue
-import src.protocol_mqtt as protocol_mqtt
+import src.can_protocol as protocol_mqtt
 
 logging.config.fileConfig('logging.conf')
 infoLogger = logging.getLogger('customInfoLogger')
